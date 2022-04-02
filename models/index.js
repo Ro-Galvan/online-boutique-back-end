0156b89cc53ba21cefb8efef968d8e35a25f08d5
ro_g <97912154+Ro-Galvan@users.noreply.github.com>
2022-04-01 20:15:32 -0400

    added all associations in models/index.js

--- models/index.js
+++ models/index.js
@@ -4,13 +4,28 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
+// ***************Associations aka relationships***************
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
 
 // Categories have many Products
-
+Category.hasMany(Product, {  //if hasMany phrase is next to it, it is the most important one than belong
+  foreignKey: 'category_id'
+}
+  )
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: 'ProductTag' //might need to change to foreignKey: 'tag_id'
+});
+//to  get 2 things to belong to each other, where neither one is primary you have to do through table.where products and tags meet
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: 'ProductTag'
+  // foreignKey: 'tag_id'
+});
 
 module.exports = {
   Product,
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
+// pull in config connection file
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -11,6 +12,9 @@ app.use(express.urlencoded({ extended: false }));  //DONT FORGET TO CHANGE BACK!
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
+
+// you need to synch before app listens or you will crash program
+
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
 });
