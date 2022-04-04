fcd21a785fc347f0e96308f1a59a4b2e7ddf3647
ro_g <97912154+Ro-Galvan@users.noreply.github.com>
2022-04-04 16:36:23 -0400

    updated belongToMany for tags and products and added unique property to be false

--- models/index.js
+++ models/index.js
@@ -12,19 +12,28 @@ Product.belongsTo(Category, {
 
 // Categories have many Products
 Category.hasMany(Product, {  //if hasMany phrase is next to it, it is the most important one than belong
-  foreignKey: 'category_id'
+  foreignKey: 'product_id'
 }
   )
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: 'ProductTag' //might need to change to through: 'ProductTag' OR  foreignKey: 'tag_id'
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  // through: 'ProductTag', //might need to change to through: 'ProductTag' OR  
+  // foreignKey: 'tag_id'
 });
 //to  get 2 things to belong to each other, where neither one is primary you have to do through table.where products and tags meet
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through: 'ProductTag'
-  // foreignKey: 'tag_id'
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  // through: 'ProductTag',
+  // foreignKey: 'product_id'
 });
 
 module.exports = {
